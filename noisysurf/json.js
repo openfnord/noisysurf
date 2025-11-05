cc09219394f2f20d55583a3c992dff0dbb632af7
openfnord <luja@openhardware.de>
2025-11-05 14:36:42 +0100

    Noizy is forked and renamed to noisysurf to be accepted as add-on

---
+++ noisysurf/json.js
@@ -0,0 +1,207 @@
+var presets = {
+  "sites": {
+    "user" : [
+    ],
+    "default" : [
+      {
+        "url": "abcnews.go.com",
+        "checked": "true"
+      },
+      {
+        "url": "aljazeera.com",
+        "checked": "true"
+      },
+      {
+        "url": "bbc.com",
+        "checked": "true"
+      },
+      {
+        "url": "berliner-zeitung.de",
+        "checked": "true"
+      },
+      {
+        "url": "bild.de",
+        "checked": "true"
+      },
+      {
+        "url": "bloomberg.com",
+        "checked": "true"
+      },
+      {
+        "url": "blick.ch",
+        "checked": "true"
+      },
+      {
+        "url": "breitbart.com",
+        "checked": ""
+      },
+      {
+        "url": "cbsnews.com",
+        "checked": "true"
+      },
+      {
+        "url": "chron.com",
+        "checked": "true"
+      },
+      {
+        "url": "cnbc.com",
+        "checked": "true"
+      },
+      {
+        "url": "cnn.com",
+        "checked": "true"
+      },
+      {
+        "url": "dailymail.co.uk",
+        "checked": "true"
+      },
+      {
+        "url": "dw.com",
+        "checked": "true"
+      },
+      {
+        "url": "derstandard.at",
+        "checked": "true"
+      },
+      {
+        "url": "drudgereport.com",
+        "checked": ""
+      },
+      {
+        "url": "forbes.com",
+        "checked": "true"
+      },
+      {
+        "url": "foxnews.com",
+        "checked": "true"
+      },
+      {
+        "url": "greekcitytimes.com",
+        "checked": "true"
+      },
+      {
+        "url": "globaltimes.cn",
+        "checked": "true"
+      },
+      {
+        "url": "huffingtonpost.com",
+        "checked": "true"
+      },
+      {
+        "url": "krone.at",
+        "checked": "true"
+      },
+      {
+        "url": "indiatimes.com",
+        "checked": "true"
+      },
+      {
+        "url": "israelhayom.com",
+        "checked": "true"
+      },
+      {
+        "url": "latimes.com",
+        "checked": "true"
+      },
+      {
+        "url": "money.cnn.com",
+        "checked": "true"
+      },
+      {
+        "url": "msnbc.com",
+        "checked": "true"
+      },
+      {
+        "url": "nationalgeographic.com",
+        "checked": "true"
+      },
+      {
+        "url": "nbcnews.com",
+        "checked": "true"
+      },
+      {
+        "url": "news.com.au",
+        "checked": "true"
+      },
+      {
+        "url": "news.yahoo.com",
+        "checked": "true"
+      },
+      {
+        "url": "nypost.com",
+        "checked": "true"
+      },
+      {
+        "url": "nytimes.com",
+        "checked": "true"
+      },
+      {
+        "url": "reddit.com",
+        "checked": ""
+      },
+      {
+        "url": "reuters.com",
+        "checked": "true"
+      },
+      {
+        "url": "scmp.com",
+        "checked": ""
+      },
+      {
+        "url": "shareblue.com",
+        "checked": ""
+      },
+      {
+        "url": "straitstimes.com",
+        "checked": ""
+      },
+      {
+        "url": "theatlantic.com",
+        "checked": "true"
+      },
+      {
+        "url": "theguardian.com",
+        "checked": "true"
+      },
+      {
+        "url": "thehill.com",
+        "checked": "true"
+      },
+      {
+        "url": "thesun.co.uk",
+        "checked": "true"
+      },
+      {
+        "url": "timesofmalta.com",
+        "checked": ""
+      },
+      {
+        "url": "time.com",
+        "checked": "true"
+      },
+      {
+        "url": "usatoday.com",
+        "checked": "true"
+      },
+      {
+        "url": "usnews.com",
+        "checked": "true"
+      },
+      {
+        "url": "welt.de",
+        "checked": "true"
+      },
+      {
+        "url": "washingtonpost.com",
+        "checked": ""
+      },
+      {
+        "url": "wsj.com",
+        "checked": "true"
+      },
+    ]
+  },
+  "enabled": "Enabled",
+  "blockStreams": false,
+  "userSitePreset": "wikipedia.org/wiki/Special:Random"
+}
